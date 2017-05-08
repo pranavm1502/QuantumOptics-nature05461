7e41a0be7e6093fe11655bfa7676fab93cab7d70
CareF <CareF.Lm@gmail.com>
2017-05-08 16:09:06 -0400

    making better figures

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 # Filename: script.py
 # Author:   Lyu Ming <CareF.Lm@gmail.com>
@@ -50,7 +50,7 @@ wrf0 = wr - chi + delta    # RF freq (pumping) ....
 def Hr(ws, wrf = wrf0): 
     return wrf * a.dag() * a + (ws / 2.0) * sz
 
-def cavity_state(erf, wrf=wrf0): 
+def cavity_state(erf, wrf=wrf0, nth=0.1): 
     """
     Return the steady state of the system
     without any sweaping signals /or qubit at ground state. 
@@ -59,10 +59,10 @@ def cavity_state(erf, wrf=wrf0):
     """
     Hrf = erf * xc
     H = H0 - Hr(0.0, wrf) + Hrf 
-    rhoss = steadystate(H, c_ops()) 
+    rhoss = steadystate(H, c_ops(nth)) 
     return rhoss
 
-def with_sweaping(erf, ws, esp=0.1):
+def with_sweaping(erf, ws, nth=0.1, esp=0.1):
     """
     Return the steady state of the system with sweaping signals.
     erf: The strength of RF signal (driving for cavity)
@@ -72,19 +72,67 @@ def with_sweaping(erf, ws, esp=0.1):
     """
     Hrf = erf * xc
     Hsp = esp * (g/Delta) * sx
-    rhoss = steadystate(H0 - Hr(ws) + Hrf + Hsp, c_ops()) 
+    rhoss = steadystate(H0 - Hr(ws) + Hrf + Hsp, c_ops(nth)) 
     return rhoss
 
-if __name__ == '__main__':
-    wslist = 2 * np.pi * np.linspace(6.75E3, 7.0E3, 500)
-    erf = 0.5
-    rho_non_sw = cavity_state(erf)
-    Ibar = (expect(a.dag() + a, rho_non_sw))**2
-    nbar = (expect(a.dag() * a, rho_non_sw))
-    print("Ibar=%f, nbar=%f"%(Ibar,nbar))
+def draw_sweaping(axis, wslist, erf, nth=0.1, obs = a.dag()+a):
+    rho_non_sw = cavity_state(erf, nth=nth)
+    Vbar = (expect(obs, rho_non_sw))
+    # nbar = (expect(a.dag() * a, rho_non_sw))
+    # print("Ibar=%f, nbar=%f"%(Ibar,nbar))
     trans = np.array([
-        expect(a.dag() + a, with_sweaping(erf, w, 0.1)) 
+        expect(obs, with_sweaping(erf, w, nth)) 
         for w in wslist]) 
-    plt.plot(wslist/(2*np.pi),  Ibar - trans**2 )
-    plt.gca().invert_xaxis()
+    axis.plot(wslist/(2*np.pi),  Vbar - trans )
+    # axis.get_yaxis().set_visible(False)
+    # axis.invert_xaxis()
+
+def coherence_drive():
+    wslist = 2 * np.pi * np.linspace(6.75E3, 7.0E3, 500)
+    nbars = np.array([0.02, 1.4, 2.3, 3.2, 4.1, 4.6])
+    erfs = np.sqrt( nbars*(delta**2 + kappa**2 / 4))
+    f, axs = plt.subplots(len(nbars), sharex=True)
+    N = len(nbars)
+    for n in range(N): 
+        draw_sweaping(axs[N-1-n], wslist, erfs[n])
+        axs[N-1-n].text(0.7, 0.9, r"$\bar n=%3f$"%nbars[n])
+        axs[N-1-n].get_yaxis().set_visible(False)
+    axs[0].invert_xaxis()
     plt.show()
+
+def thermal():
+    wslist = 2 * np.pi * np.linspace(6.75E3, 7.0E3, 500)
+    f, axs = plt.subplots(2, sharex=True)
+    draw_sweaping(axs[0], wslist, np.sqrt( 2.9*(delta**2 + kappa**2 / 4)))
+    axs[0].get_yaxis().set_visible(False)
+    draw_sweaping(axs[1], wslist, 0.01, nth=3)
+    axs[1].get_yaxis().set_visible(False)
+    axs[0].invert_xaxis()
+    plt.show()
+
+# def wignar_diff():
+
+
+if __name__ == '__main__':
+    # rhoc = cavity_state(30, wrf0).ptrace(0)
+    # plt.bar(np.arange(0, N), rhoc.diag())
+    # plt.show()
+
+    # wrflist = wrf0 + np.linspace(-80, 80, 500)
+    # nbarlist = [expect(a.dag()*a, cavity_state(20, wrf)) for wrf in wrflist]
+    # plt.plot(wrflist/(2*np.pi), 20**2/(kappa**2/4 + (delta + wrflist-wrf0)**2)+0.1, '--')
+    # plt.plot(wrflist/(2*np.pi), nbarlist)
+    # plt.ylim((-0.3, 13))
+    # plt.arrow(wrf0/(2*np.pi), 4.5, 0, -0.8, ec='r', fc= 'r', head_width=0.4, head_length=0.5)
+    # plt.xlabel(r"$\omega_{\mathrm{rf}}/(2\pi)$ (MHz)")
+    # plt.ylabel(r"$\langle a^\dagger a\rangle$")
+    # plt.show()
+
+    wslist = 2 * np.pi * np.linspace(6.75E3, 7.0E3, 500)
+    # draw_sweaping(plt.gca(), wslist, 0.1, obs=a.dag()*a)
+    # # plt.gca().get_yaxis().set_visible(False)
+    # plt.gca().invert_xaxis()
+    # plt.xlabel(r"$\omega_s/(2\pi)$ (MHz)", fontsize=12)
+    # plt.ticklabel_format(style='sci', axis='y', scilimits=(-3,4))
+    # plt.ylabel(r"$\bar n_s - \bar n$", fontsize=12)
+    # plt.show()
